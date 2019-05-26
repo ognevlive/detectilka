ed4335d74612c3c96a0c7160ce0d39c05ca3a040
ognevlive <ognevlive@mail.ru>
2019-05-25 23:52:08 -0700

    change filename to insensitive string

--- app/models.py
+++ app/models.py
@@ -38,7 +38,7 @@ def load_user(id):
 class Sample(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    filename = db.Column(db.String(128), index=True)
+    filename = db.Column(db.String(128, collation='NOCASE'), index=True)
     user_id   = db.Column(db.Integer, db.ForeignKey('user.id'))
     hash      = db.Column(db.String(128), index=True)
     answer    = db.Column(db.String(16), index=True)
